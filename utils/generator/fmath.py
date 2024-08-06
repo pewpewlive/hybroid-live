c7b2156c39f7a433c88134b814fcf9285be696dc
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2024-08-06 14:19:18 +0300

    update fmath generator

--- utils/generator/fmath.py
+++ utils/generator/fmath.py
@@ -1,6 +1,20 @@
 def generate_api(fmath_lib: dict) -> str:
     return "package walker"
 
+_FMATH_DOCS_TEMPLATE = """---
+title: Fmath API
+slug: appapi/fmath
+sidebar:
+  order: 2
+---
+
+<!-- This is an auto-generated file. To modify it, change utils/generate_api.py in Hybroid's repository. -->
+
+## Functions
+
+%s
+"""
+
 
 def generate_docs(fmath_lib: dict) -> str:
-    return "# Fmath"
+    return _FMATH_DOCS_TEMPLATE % ("To Be Added.")
