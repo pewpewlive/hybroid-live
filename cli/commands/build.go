61981f420043b5075a1699535c4839a74ab073b7
SKPG-Tech <42890752+SKPG-Tech@users.noreply.github.com>
2025-07-10 19:55:57 +0300

    Add custom `#{xxx}` and `#{xxxxxxxx}` color formats for level name
    Co-authored-by: Mutoxicated <Mutoxicated@users.noreply.github.com>
    Co-authored-by: Dominykas M. <Tasty-Kiwi@users.noreply.github.com>
    Co-authored-by: MnHs <RanPix@users.noreply.github.com>

--- cli/commands/build.go
+++ cli/commands/build.go
@@ -7,6 +7,7 @@ import (
 	"hybroid/evaluator"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/pelletier/go-toml/v2"
 	"github.com/urfave/cli/v2"
@@ -19,42 +20,74 @@ func Build() *cli.Command {
 		Usage:       "Builds a Hybroid Live project",
 		Description: "This will take the current project in the location the command was ran, and will transpile the project into its destination folder, based on the config file",
 		Action: func(ctx *cli.Context) error {
-			return Build_(ctx)
+			return Build_()
 		},
 	}
 }
 
-func runEvaluator(config core.HybroidConfig, filesToBuild []core.FileInformation, cwd string, err chan<- error) {
+func runEvaluator(config core.HybroidConfig, filesToBuild []core.FileInformation, cwd string) error {
 	outputDir := config.Project.OutputDirectory
 
 	if outputDir != "" {
 		os.MkdirAll(cwd+outputDir, os.ModePerm)
 	}
 
+	regex := regexp.MustCompile(`(?:#\{[0-9a-fA-F]+\})|(?:#[0-9a-fA-F]{1,8})`)
+	matches := regex.FindAllStringSubmatchIndex(config.Level.Name, -1)
+	index := -1
+	var err error
+	config.Level.Name = string(regex.ReplaceAllFunc([]byte(config.Level.Name), func(b []byte) []byte {
+		index++
+		var color []byte
+		if b[0] == '#' && b[1] == '{' {
+			color = append([]byte{'#'}, b[2:len(b)-1]...)
+			if len(color) == 5 {
+				color = []byte{'#', color[1], color[1], color[2], color[2], color[3], color[3], color[4], color[4]}
+			}
+		} else {
+			color = b
+		}
+		colorLen := len(color)
+		if colorLen != 9 {
+			err = fmt.Errorf("color '%s' at around %d-%d in level title must be either 4 characters (braced) or 8 characters (braced/raw)", b, matches[index][0]+9, matches[index][1]+9)
+		}
+		//fmt.Printf("%s\n", b)
+		return color
+	}))
+	if err != nil {
+		return err
+	}
+
 	manifestConfig := config.Level
 	manifestConfig.EntryPoint = "level.lua"
 	manifestConfig.IsCasual = !config.Level.IsCasual
-	manifest, manifestErr := json.MarshalIndent(manifestConfig, "", "  ")
-	if manifestErr != nil {
-		err <- fmt.Errorf("failed creating level manifest file: %v", manifestErr)
-		return
-	}
-	os.WriteFile(filepath.Join(cwd, outputDir, "/manifest.json"), manifest, os.ModePerm)
 
 	if len(filesToBuild) == 0 {
 		files, filesErr := core.CollectFiles(cwd)
 		if filesErr != nil {
-			err <- filesErr
-			return
+			return filesErr
+
 		}
 		filesToBuild = append(filesToBuild, files...)
 	}
 
 	evaluator := evaluator.NewEvaluator(filesToBuild)
-	err <- evaluator.Action(cwd, outputDir)
+	err = evaluator.Action(cwd, outputDir)
+	if err != nil {
+		return err
+	}
+
+	manifest, manifestErr := json.MarshalIndent(manifestConfig, "", "  ")
+	if manifestErr != nil {
+		return fmt.Errorf("failed creating level manifest file: %v", manifestErr)
+
+	}
+	os.WriteFile(filepath.Join(cwd, outputDir, "/manifest.json"), manifest, os.ModePerm)
+
+	return nil
 }
 
-func Build_(ctx *cli.Context, filesToBuild ...core.FileInformation) error {
+func Build_(filesToBuild ...core.FileInformation) error {
 	cwd, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("failed getting current working directory: %v", err)
@@ -71,10 +104,9 @@ func Build_(ctx *cli.Context, filesToBuild ...core.FileInformation) error {
 		return fmt.Errorf("failed parsing Hybroid Live config file: %v", err)
 	}
 
-	evalError := make(chan error)
-	go runEvaluator(config, filesToBuild, cwd, evalError)
-	if err = <-evalError; err != nil {
-		return fmt.Errorf("failed evaluation: %v", err)
+	err = runEvaluator(config, filesToBuild, cwd)
+	if err != nil {
+		return fmt.Errorf("build failed: %v", err)
 	}
 
 	return nil
