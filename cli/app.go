dc339b0c8b6f02198a3219c0de20a154177944a3
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2026-02-12 21:02:28 +0200

    Update copyright year to 2026 and other touch-ups

--- cli/app.go
+++ cli/app.go
@@ -13,13 +13,12 @@ func RunApp() {
 		Name:      "hybroid-live",
 		Usage:     "The Hybroid Live transpiler CLI",
 		Version:   "0.1.0",
-		Copyright: "Copyright (C) Hybroid Team, 2025\nLicensed under Apache-2.0",
+		Copyright: "© Hybroid Team, 2026\nLicensed under Apache-2.0",
 		Commands: []*cli.Command{
 			commands.Add(),
 			commands.Build(),
 			commands.Initialize(),
 			commands.Watch(),
-			// LSP is not yet implemented
 			commands.Lsp(),
 		},
 	}
