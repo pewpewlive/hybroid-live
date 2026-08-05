2ede672a77521853449849dba2c59e9b950632d8
SKPG-Tech <42890752+SKPG-Tech@users.noreply.github.com>
2026-08-05 18:53:19 +0300

    [alerts] Update `generate_alerts.py` script to follow new format

--- utils/alerts/helpers.py
+++ utils/alerts/helpers.py
@@ -21,17 +21,33 @@ def to_receiver(original: str) -> str:
 type Format = dict[str, str] | str
 
 
-def format_string(string: str, string_format: list[Format], receiver: str) -> str:
+def format_string(
+    string: str,
+    string_format: list[Format],
+    receiver: str,
+    accessors: dict[str, str] | None = None,
+) -> str:
+    if accessors is None:
+        accessors = {}
+
     if len(string_format) == 0:
         return f'"{string}"'
 
     specifiers = []
     for specifier in string_format:
         if type(specifier) is str:
-            specifiers.append(f"{receiver}.{specifier}")
+            specifiers.append(f"{receiver}.{accessors.get(specifier, specifier)}")
         elif type(specifier) is dict:
             specifier, format = dict(specifier).popitem()
             imports.update_imports(format)
-            specifiers.append(format.format(f"{receiver}.{specifier}"))
+            specifiers.append(
+                format.format(f"{receiver}.{accessors.get(specifier, specifier)}")
+            )
 
     return f'fmt.Sprintf("{string}", {", ".join(specifiers)})'
+
+
+def to_param_name(name: str) -> str:
+    if not name:
+        return name
+    return name[0].lower() + name[1:]
